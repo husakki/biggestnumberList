6f9836b61ed54210b47b42e5f69f7568ba7aedd3
husakki <hueschromo@gmail.com>
2026-04-30 16:17:22 +0200

    working biggest number!!!!

--- bnl.erl
+++ bnl.erl
@@ -9,5 +9,11 @@ maxList(List) ->
     [Head | Tail] = List,
     maxList(Tail,Head).
 
-maxList(List, Biggest) ->
-    ok.
+maxList([Head|List], Biggest) when Head > Biggest ->
+    maxList(List, Head);
+
+maxList([_| Tail] , Biggest) ->
+    maxList(Tail, Biggest);
+
+maxList([],Biggest) ->
+    io:format("The biggest number is: ~p\n",[Biggest]).
